493258abb5785b8c3e5bb0a568039e27730b3bf1
AhmadrezaYousefkhani <yahmadrez@student.ethz.ch>
2020-03-09 18:02:48 +0100

    Repeated k-fold

--- task_1b/Ldt_1b.py
+++ task_1b/Ldt_1b.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[175]:
+# In[7]:
 
 
 import numpy as np
@@ -13,9 +13,10 @@ from sklearn.model_selection import cross_validate
 from sklearn.linear_model import RidgeCV
 from sklearn.preprocessing import StandardScaler
 from sklearn import preprocessing
+from sklearn.model_selection import RepeatedKFold
 
 
-# In[4]:
+# In[3]:
 
 
 dat = pd.read_csv("./data/train.csv", header = 0)
@@ -23,7 +24,7 @@ y = dat['y']
 X = dat.iloc[:,2:7]
 
 
-# In[167]:
+# In[4]:
 
 
 fm2 = np.cos(X)
@@ -35,70 +36,50 @@ frames = [X, fm3, fm4, fm2, fm5]
 dat = pd.concat(frames, axis = 1)
 
 
-# In[168]:
+# In[5]:
 
 
-# X.shape
-# fm2.shape
-
-# fm3.shape
-
-# fm4.shape
-
-# fm5.shape
 num = list(range(1,22))
 res = list(map(str,num)) 
 res = np.core.defchararray.add("V", res)
 dat.columns = res
 
 
-# In[177]:
-
-
-
-
-
-# In[178]:
+# In[23]:
 
 
 ridgr_parameter = [0.001, 0.01, 0.1, 1, 1.1, 1.4, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 7, 8, 9, 10, 15, 20, 30, 50, 70, 100]
 coefs = []
+score = []
+max_score = -np.Inf
+bestregressor = []
+
 for i in range(len(frames)):
+#     quantile_transformer = preprocessing.QuantileTransformer(random_state=0)
+#     X_scaled = quantile_transformer.fit_transform(frames[i])
+#     min_max_scaler = preprocessing.MinMaxScaler()
+#     X_scaled = min_max_scaler.fit_transform(frames[i])
     X_scaled = preprocessing.scale(frames[i])
     
-    tmpRidge = RidgeCV(alphas= ridgr_parameter, fit_intercept= False, normalize= True, cv = 10).fit(X_scaled, y)
-    print(tmpRidge.score(X_scaled, y))
-    print(tmpRidge.alpha_)
+    rkf = RepeatedKFold(n_splits= 10, n_repeats= 10, random_state= 1234)
+    max_score = -np.Inf
+    for train_index, test_index in rkf.split(X):
+        X_train, X_test = X_scaled[train_index], X_scaled[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        tmpRidge = RidgeCV(alphas= ridgr_parameter, fit_intercept= False, normalize= True).fit(X_train, y_train)
+        tmp_score = tmpRidge.score(X_test, y_test)
+        score.append(tmpRidge.score(X_test, y_test))
+        if(max_score < tmp_score):
+            bestregressor = tmpRidge
+        
     
-    coefs.extend(tmpRidge.coef_)
-    print(coefs)
+    coefs.extend(bestregressor.coef_)
 
 
-# In[180]:
+# In[24]:
 
 
-#np.savetxt("results.csv", tmpRidge.coef_, delimiter=",")
+print(len(coefs))
 list(coefs)
-np.savetxt("results.csv", coefs, delimiter=",")
-
-
-# In[159]:
-
-
-ridgr_parameter = [0.001, 0.01, 0.1, 1, 1.1, 1.4, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 10,15]
-tmpRidge = RidgeCV(alphas= ridgr_parameter, fit_intercept= False, normalize= True, cv = 10).fit(dat, y)
-tmpRidge.score(dat, y)
-tmpRidge.coef_
-#tmp = linear_model.Ridge(alpha = ridgr_parameter[3], normalize= True, fit_intercept = False,
-#                                     max_iter=100000, tol = 1e-8, solver = 'auto'))
-
-np.savetxt("results.csv", coefs, delimiter=",")
-
-
-# In[160]:
-
-
-print(tmpRidge.alpha_)
-tmpRidge.score(dat, y)
-tmpRidge.coef_
+#np.savetxt("results.csv", coefs, delimiter=",")
 
