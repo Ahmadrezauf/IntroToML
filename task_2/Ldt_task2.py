bdc8154be8703f7ac1966935df688040c5d82f84
AhmadrezaYousefkhani <yahmadrez@student.ethz.ch>
2020-04-26 12:45:38 +0200

    Imputing and scaling the test separately

--- task_2/Ldt_task2.py
+++ task_2/Ldt_task2.py
@@ -3,7 +3,7 @@
 
 # # Support vector machines
 
-# In[16]:
+# In[17]:
 
 
 # import libraries
@@ -16,6 +16,7 @@ import missingno as msno
 import seaborn as sns
 
 from matplotlib.backends.backend_pdf import PdfPages
+from sklearn.decomposition import PCA
 
 from sklearn.impute import SimpleImputer
 from sklearn import preprocessing
@@ -58,10 +59,10 @@ df_test_features = pd.read_csv ('test_features.csv')
 
 df_train_labels.hist()
 
-with PdfPages("./Results/Labels_histogram.pdf") as export_pdf:
-    for i in list(df_train_labels)[1:]:
-        df_train_labels.hist(column = i, bins = 100)
-        export_pdf.savefig()
+# with PdfPages("./Results/Labels_histogram.pdf") as export_pdf:
+#     for i in list(df_train_labels)[1:]:
+#         df_train_labels.hist(column = i, bins = 100)
+#         export_pdf.savefig()
 
 
 # One can see the class imbalance problem here. Other observations:
@@ -73,7 +74,7 @@ with PdfPages("./Results/Labels_histogram.pdf") as export_pdf:
 
 # ### Train Data pre-processing
 
-# In[32]:
+# In[3]:
 
 
 # data inspection: 
@@ -100,7 +101,7 @@ ax.set_xticklabels(
 #         export_pdf.savefig()
 
 
-# In[24]:
+# In[4]:
 
 
 # calculate the correlation matrix
@@ -137,37 +138,75 @@ msno.heatmap(df_train_features)
 
 # ### Train data pre-processing
 
-# In[33]:
+# In[6]:
 
 
-# THIS STEP REMOVES ALL NAN !!!
+# Patient by patient pre-processing for imputation and feature generation
 
-# aggregate data for each pid
-# GROUPBY REARRANGES THE ROWS, WE HAVE TO DO THE SAME FOR THE LABELS
-df_train_aggregate_features = df_train_features.groupby('pid').agg('median')
+# get variables from train features
+variables = df_train_features.columns[2:]
 
-# print(df_train_aggregate_features)
+# get all pids, to impute and pre-process individually
+train_pids = df_train_features['pid'].unique()
+
+for pid in train_pids:
+    # dataframe for this pid
+    df = df_train_features.loc[df_train_features['pid'] == pid]
+    
+    for var in variables:
+        # how many NaN are in there for this variable
+        data = df[var]
+        num_nan = data.isnull().sum(axis=0)
+        if num_nan == 12:
+            continue
+        
+        data = data.to_numpy()
+           
+        imp = SimpleImputer(missing_values = np.nan)
+        train_imputed = imp.fit_transform(data.reshape(-1, 1))
+    
+        df_train_features.loc[df_train_features['pid'] == pid,var] = train_imputed
 
 
-# In[34]:
+# In[8]:
 
 
-# remove time from data frame 
-df_train_agg_features = df_train_aggregate_features.drop(['Time'], axis = 1)
-# print(df_train_agg_features)
+# aggregate the time series
+data_array = np.empty([len(train_pids), len(variables)])
+i = 0
+j = 0
+
+for pid in train_pids:
+    
+    df = df_train_features.loc[df_train_features['pid'] == pid]
+    j = 0
+    
+    for var in variables:
+        # how many NaN are in there for this variable
+        data = df[var]
+        num_nan = data.isnull().sum(axis=0)
+        if num_nan == 12:
+            data_array[i, j] = np.nan
+            j = j + 1
+            continue
+        data = data.to_numpy()
+        data_array[i, j] = np.mean(data)
+        j = j + 1 
+        
+    i = i +1
 
 
-# In[35]:
+# In[9]:
 
 
 # impute missing data points
 #imp = SimpleImputer(strategy="mean")
 imputer = KNNImputer(n_neighbors=12)
-df_train_agg_imputed_features = imputer.fit_transform(df_train_agg_features)
+df_train_agg_imputed_features = imputer.fit_transform(data_array)
 #print(df_train_agg_imputed_features)
 
 
-# In[48]:
+# In[10]:
 
 
 # scale the data
@@ -177,7 +216,7 @@ min_max_scaler = preprocessing.StandardScaler()
 data_train_scaled = min_max_scaler.fit_transform(df_train_agg_imputed_features)
 
 
-# In[49]:
+# In[11]:
 
 
 # REARRANGE THE LABELS, TO MATCH THE REARRANGED FEATURES
@@ -185,7 +224,7 @@ df_train_labels_sorted = df_train_labels.sort_values(by = 'pid')
 # print(df_train_labels_sorted)
 
 
-# In[50]:
+# In[12]:
 
 
 # Visualizing the training data after imputing and aggregating
@@ -199,16 +238,46 @@ ax.set_xticklabels(
 );
 
 
-# In[51]:
+# In[15]:
 
 
 # What is the correlation between the 
 pd.DataFrame(data_train_scaled).corrwith(other = pd.DataFrame(df_train_agg_imputed_features), method = "spearman").transpose()
 
 
+# ### PCA plot 
+
+# In[18]:
+
+
+pca = PCA(n_components=2)
+
+principalComponents = pca.fit_transform(data_train_scaled)
+principalDf = pd.DataFrame(data = principalComponents
+             , columns = ['principal component 1', 'principal component 2'])
+
+finalDf = pd.concat([principalDf, df_train_labels[['LABEL_BaseExcess']]], axis = 1)
+
+fig = plt.figure(figsize = (8,8))
+ax = fig.add_subplot(1,1,1) 
+ax.set_xlabel('Principal Component 1', fontsize = 15)
+ax.set_ylabel('Principal Component 2', fontsize = 15)
+ax.set_title('2 component PCA for LABEL_BaseExcess', fontsize = 20)
+targets = [0, 1]
+colors = ['r', 'g', 'b']
+for target, color in zip(targets,colors):
+    indicesToKeep = finalDf['LABEL_BaseExcess'] == target
+    ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
+               , finalDf.loc[indicesToKeep, 'principal component 2']
+               , c = color
+               , s = 50)
+ax.legend(targets)
+ax.grid()
+
+
 # ### Test Data pre-processing
 
-# In[52]:
+# In[20]:
 
 
 # data inspection: 
@@ -221,46 +290,104 @@ print("number of missing values:")
 print(df_test_features.isnull().sum(axis=0))
 
 
-# In[53]:
+# In[33]:
 
 
-# aggregate data for each pid
-df_test_aggregate_features = df_test_features.groupby('pid').agg('median')
+# # aggregate data for each pid
+# df_test_aggregate_features = df_test_features.groupby('pid').agg('median')
 
-#print(df_test_aggregate_features)
+# #print(df_test_aggregate_features)
 
-# collect all test pids
+# # collect all test pids
 test_pids = list(set(df_test_features.pid))
 
 
-# In[54]:
+# In[34]:
 
 
-# remove time from data frame 
-df_test_agg_features = df_test_aggregate_features.drop(['Time'], axis = 1)
-# print(df_test_agg_features)
+# Patient by patient pre-processing for imputation and feature generation
+
+# get variables from train features
+variables_test = df_test_features.columns[2:]
+
+# get all pids, to impute and pre-process individually
+test_pids = list(set(df_test_features.pid))
+
+for pid in test_pids:
+    # dataframe for this pid
+    df = df_test_features.loc[df_test_features['pid'] == pid]
+    
+    for var in variables_test:
+        # how many NaN are in there for this variable
+        data = df[var]
+        num_nan = data.isnull().sum(axis=0)
+        if num_nan == 12:
+            continue
+        
+        data = data.to_numpy()
+           
+        imp = SimpleImputer(missing_values = np.nan)
+        test_imputed = imp.fit_transform(data.reshape(-1, 1))
+    
+        df_test_features.loc[df_test_features['pid'] == pid,var] = test_imputed
+
+# aggregate the time series
+data_array = np.empty([len(test_pids), len(variables_test)])
+i = 0
+j = 0
+
+for pid in test_pids:
+    
+    df = df_test_features.loc[df_test_features['pid'] == pid]
+    j = 0
+    
+    for var in variables:
+        # how many NaN are in there for this variable
+        data = df[var]
+        num_nan = data.isnull().sum(axis=0)
+        if num_nan == 12:
+            data_array[i, j] = np.nan
+            j = j + 1
+            continue
+        data = data.to_numpy()
+        data_array[i, j] = np.mean(data)
+        j = j + 1 
+        
+    i = i +1
 
 
-# In[55]:
+# In[36]:
+
+
+# # remove time from data frame 
+# df_test_agg_features = df_test_aggregate_features.drop(['Time'], axis = 1)
+print(df_test_agg_features)
+print(data_train_scaled.shape)
+
+
+# In[38]:
 
 
 # impute missing data points
 # should we impute it with the same imputer that we've used for train?
-df_test_agg_imputed_features = imputer.transform(df_test_agg_features)
+
+imputer = KNNImputer(n_neighbors=8)
+df_test_agg_imputed_features = imputer.fit_transform(data_array)
 
 
-# In[56]:
+# In[41]:
 
 
 # scale test data
-data_test_scaled = min_max_scaler.transform(df_test_agg_imputed_features)
+min_max_scaler = preprocessing.StandardScaler()
+data_test_scaled = min_max_scaler.fit_transform(df_test_agg_imputed_features)
 
 
 # ## Fit a model & Predict
 
 # ### predict with support vector machine classification and use probabilities
 
-# In[57]:
+# In[43]:
 
 
 # first for the labels that have an output [0,1]
@@ -268,8 +395,8 @@ data_test_scaled = min_max_scaler.transform(df_test_agg_imputed_features)
 columns_1 = [test_pids]
 
 for i in range(1, 12):
-    clf = SVC(kernel = 'poly', degree = 5, class_weight = 'balanced', verbose = True)
-    clf.fit(data_train_scaled, df_train_labels_sorted.iloc[:,i])
+    clf = SVC(kernel = 'poly', degree = 3, class_weight = 'balanced', verbose = True)
+    clf.fit(data_train_scaled, df_train_labels.iloc[:,i])
     # pred = clf.predict(df_test_agg_imputed_features)
     # columns_1.append(pred)
      
@@ -278,7 +405,10 @@ for i in range(1, 12):
     distance_hyperplane = clf.decision_function(data_test_scaled)
     probability = np.empty(len(distance_hyperplane))
     for j in range(0, len(probability)):
-        probability[j] = 1 / (1 + math.exp(- distance_hyperplane[j]))
+        if distance_hyperplane[j] < 0:
+            probability[j] = 1 - 1/(1 + math.exp(distance_hyperplane[j]))
+        else:
+            probability[j] = 1/(1 + math.exp(-distance_hyperplane[j]))
     columns_1.append(probability)
 
 
@@ -286,24 +416,27 @@ for i in range(1, 12):
     distance_hyperplace_train = clf.decision_function(data_train_scaled)
     probability = np.empty(len(distance_hyperplace_train))
     for j in range(0, len(probability)):
-        probability[j] = 1 / (1 + math.exp(- distance_hyperplace_train[j]))
+        if distance_hyperplace_train[j] < 0:
+            probability[j] = 1 - 1/(1 + math.exp(distance_hyperplace_train[j]))
+        else:
+            probability[j] = 1/(1 + math.exp(-distance_hyperplace_train[j]))
       
     tmp = roc_auc_score(y_score= probability, y_true= df_train_labels.iloc[:,i])
     print("ROC AUC for feature", list(df_train_labels)[i] , " : ", tmp)
     
 
 
-# In[67]:
+# In[44]:
 
 
 # labels that have a real value
 columns_2 = []
 
 for i in range(12, 16):
-    clf_w = SVR(degree = 3)
-    parameters = {'kernel':('poly', 'rbf', 'linear'), 'C':np.linspace(1,10, 10)}
-    clf = model_selection.GridSearchCV(estimator= clf_w, param_grid = parameters, cv = 10,
-                                       refit = True, scoring = 'r2', verbose = 1)
+    clf_w = SVR(kernel = 'poly', degree =3)
+    parameters = {'C':np.linspace(1,10, 5)}
+    clf = model_selection.GridSearchCV(estimator= clf_w, param_grid = parameters, cv = 4,
+                                       refit = True, scoring = 'r2', verbose = 1, n_jobs=6)
     clf.fit(data_train_scaled, df_train_labels.iloc[:,i])
     pred_train = clf.predict(data_train_scaled)
     tmp = r2_score(y_pred= pred_train, y_true=df_train_labels.iloc[:,i])
@@ -312,10 +445,9 @@ for i in range(12, 16):
     pred = clf.predict(data_test_scaled)
     columns_2.append(pred)
     
-    break
 
 
-# In[ ]:
+# In[45]:
 
 
 columns_final = columns_1 + columns_2
@@ -379,7 +511,7 @@ columns_final = columns_1 + columns_2
 
 # ## Save predictions
 
-# In[83]:
+# In[46]:
 
 
 print(np.shape(columns_final))
